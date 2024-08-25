fb66414798e59fda735aca3329f2344cdd25756c
Radee Khan <khanradee318@gmail.com>
2024-08-25 12:52:11 -0400

    Added Flag Viewing and button animations

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+const option3 = document.getElementById('option3');
+const option4 = document.getElementById('option4');
+
